1d9893dc7d31f64bbb1bc94c010f37e1da52d3fc
aaxxiiss <mail@jukkaisokoski.fi>
2021-10-14 23:18:22 +0300

    added open modal link to images and made them more accessable

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -6,11 +6,15 @@ export default function PortfolioItem({ item, openModal }) {
   return (
     <article className={articleClassNames}>
       <div className="content">
-        <img
+        <input
+          type="image"
           className="image"
           src={item.meta.cover_image}
           alt={item.meta.title}
+          onClick={() => openModal(item)}
+          aria-label={'Read more about ' + item.meta.title}
         />
+
         <p className="description">{item.meta.description}</p>
         <h3 className="heading-3">{item.meta.title}</h3>
         <ul className="tech-tags">
